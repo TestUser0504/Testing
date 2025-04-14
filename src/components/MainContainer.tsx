b2a9b2c5dc59b6d7dce4fd0aa1894156cf3ae631
TestUser0504 <hackursahas@gmail.com>
2025-04-15 01:23:15 +0530

    some more changes

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -8,7 +8,7 @@ import Navbar from "./Navbar";
 import SocialIcons from "./SocialIcons";
 import WhatIDo from "./WhatIDo";
 import Work from "./Work";
-import setSplitText from "./utils/splitText";
+// import setSplitText from "./utils/splitText";
 
 // const TechStack = lazy(() => import("./TechStack"));
 
@@ -19,7 +19,7 @@ const MainContainer = ({ children }: PropsWithChildren) => {
 
   useEffect(() => {
     const resizeHandler = () => {
-      setSplitText();
+      // setSplitText();
       setIsDesktopView(window.innerWidth > 1024);
     };
     resizeHandler();
